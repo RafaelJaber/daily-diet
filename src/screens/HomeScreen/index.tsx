9799543da3faab916b6ae8e60d76569b1fd8b1a1
Rafael Jáber <rafael.jaber@hotmail.com>
2024-04-17 19:00:15 -0300

    implementação: implementado a HomeScreen da aplicação.

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,15 +1,52 @@
-import { Text } from "react-native";
+import { format } from "date-fns";
+import { SectionList } from "react-native";
 
+import { ButtonComponent } from "@/components/ButtonComponent";
+import { CardSummary } from "@/components/CardSummary";
 import { HeaderComponent } from "@/components/HeaderComponent";
+import { ItemListComponent } from "@/components/ItemListComponent";
+import { SnackListModel } from "@/models/snack-list.model";
 
-import { AreaView, Container } from "./styles";
+import { AreaView, Container, ListHeader, TextMD } from "./styles";
+
+const listMock: SnackListModel[] = [
+  {
+    date: new Date(),
+    data: [
+      {
+        name: "Sanduiche",
+        description: "Sanduiche cabuloso",
+        withinTheDiet: false,
+        date: new Date(),
+      },
+      {
+        name: "Sorvete",
+        description: "Sorvete cabuloso",
+        withinTheDiet: true,
+        date: new Date(),
+      },
+    ],
+  },
+];
 
 export function HomeScreen() {
   return (
     <AreaView>
       <HeaderComponent />
       <Container>
-        <Text>Teste de conteúdo</Text>
+        <CardSummary percent={80.8} minimalPercent={80.9} />
+
+        <TextMD>Refeições</TextMD>
+        <ButtonComponent text="Nova refeição" icon={"add"} />
+
+        <SectionList
+          sections={listMock}
+          keyExtractor={(item, index) => item.name + index}
+          renderItem={({ item }) => <ItemListComponent snack={item} />}
+          renderSectionHeader={({ section: { date } }) => (
+            <ListHeader>{format(date, "dd.MM.yy")}</ListHeader>
+          )}
+        />
       </Container>
     </AreaView>
   );
--- src/screens/HomeScreen/styles.ts
+++ src/screens/HomeScreen/styles.ts
@@ -1,5 +1,5 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const AreaView = styled(SafeAreaView)`
   flex: 1;
@@ -10,4 +10,25 @@ export const Container = styled.View`
   flex: 1;
 
   padding: 24px;
+`;
+
+export const TextMD = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.GRAY_1};
+  `};
+  text-align: left;
+  margin-top: 40px;
+  margin-bottom: 8px;
+`;
+
+export const ListHeader = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.LG}px;
+    color: ${theme.COLORS.GRAY_1};
+  `};
+  margin-top: 32px;
+  margin-bottom: 8px;
 `;
